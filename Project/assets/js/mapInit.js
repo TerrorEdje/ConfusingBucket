96b56c9ff8bd5172a58b338fcbcf06a985083273
RobinCollard <name24@hotmail.nl>
2014-02-12 12:28:45 +0100

    Google map to Hybrid and avans added

--- Project/assets/js/mapInit.js
+++ Project/assets/js/mapInit.js
@@ -1,11 +1,24 @@
 var map;
+var avansLatlng = new google.maps.LatLng(51.688946, 5.287256);
+var centerLatlng = new google.maps.LatLng(0,0);
+
 function initialize() {
 	var mapOptions = {
 		zoom: 2,
-		center: new google.maps.LatLng(0,0)
+		minZoom: 2,
+		center: centerLatlng,
+		mapTypeId: google.maps.MapTypeId.HYBRID
 	};
 	
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	
+	var marker = new google.maps.Marker({
+      position: avansLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+  
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
