25fa49ceecdcaa398df4c292c658d42f9389206b
Thomas Voogt <chirimorin@gmail.com>
2014-02-12 15:29:04 +0100

    Opleiding model

---
+++ Project/model/opleiding.php
@@ -0,0 +1,22 @@
+<?php
+	class Opleiding
+	{
+		private $id;
+		private $naam;
+		private $omschrijving;
+		private $organisatie_id;
+		
+		public function _get($property) {
+			if (property_exists($this, $property)) {
+				return $this->$property;
+			}
+		}
+		
+		public function _set($property, $value) {
+			if (property_exists($this, $property)) {
+				$this->$property = $value;
+			}
+			return $this;
+		}
+	}
+?>
