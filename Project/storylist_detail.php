02292b764e84d5a9354737b48518d51c55bf87f7
michael van de Ven <michael.vd.ven@hotmail.com>
2014-02-17 12:19:27 +0100

    aanpassing nog maals

--- Project/storylist_detail.php
+++ Project/storylist_detail.php
@@ -7,22 +7,22 @@
 	if($_GET['storyid'] != null)
 	{
 		$story = getStoryByID($_GET['storyid'], $con);
+		
+		if($story != null)
+		{
+			echo "<h2>Stage gelopen van ".$story -> _get("begin_datum")." tot ".$story -> _get("eind_datum")."</h2>";
+			echo "<h3>".$story -> _get("beschrijving")."</h3>";
+			echo "<h3>Ik heb dit gedaan in leerjaar ".$story -> _get("leerjaar")."</h3>";
+			echo "<h4><a href='".$story -> _get("link")."'>".$story -> _get("link")."</a></h4>";
+		}
+		else
+		{
+			echo "<h3>Dit verhaal is helaas niet langer beschikbaar</h3>";
+		}
 	}
 	else
 	{
 		echo "<h3>Je aanpassing in de url balk is niet helemaal goed gegaan ;)</h3>";
-	}
-	
-	if($story != null)
-	{
-		echo "<h2>Stage gelopen van ".$story -> _get("begin_datum")." tot ".$story -> _get("eind_datum")."</h2>";
-		echo "<h3>".$story -> _get("beschrijving")."</h3>";
-		echo "<h3>Ik heb dit gedaan in leerjaar ".$story -> _get("leerjaar")."</h3>";
-		echo "<h4><a href='".$story -> _get("link")."'>".$story -> _get("link")."</a></h4>";
-	}
-	else
-	{
-		echo "<h3>Dit verhaal is helaas niet langer beschikbaar</h3>";
 	}
 	
 	closeDB($con);
