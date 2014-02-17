754bee9df8b6711cc803b7e3586fc5a2cf85c6b9
Thomas Voogt <chirimorin@gmail.com>
2014-02-17 12:38:23 +0100

    Markers van PHP array

--- Project/views/shared/head.php
+++ Project/views/shared/head.php
@@ -12,8 +12,17 @@
 	
 	<script type="text/javascript">
 		var locations = new Array();
-		
-		locations.push( {  id:"1", lat:51.688946, lng:5.287256, title:"Avans hogeschool" } );
+		<?php
+			$locations = array();
+			
+			$locations[0] = (object) array('id' => '0', 'lat' => '51.688946', 'lng'=>'5.287256', 'title'=>'Avans hogeschool');
+			$locations[1] = (object) array('id' => '1', 'lat' => '51.67855', 'lng'=>'5.12173', 'title'=>'Robins huis');
+			
+			foreach ($locations as $location)
+			{
+				echo("locations.push( {  id:\"".$location->id."\", lat:".$location->lat.", lng:".$location->lng.", title:\"".$location->title."\" }); ");
+			}
+		?>
 	</script>
 	
 	<script type="text/javascript" src="assets/js/mapInit.js"></script>
