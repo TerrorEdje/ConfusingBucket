f4b026d47e64c0c4c0035b1ba3fc8dde3de46c93
Thomas Voogt <chirimorin@gmail.com>
2014-02-17 11:49:43 +0100

    css fix

--- Project/views/shared/head.php
+++ Project/views/shared/head.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
 	
-	<link rel="stylesheet" href="../assets/css/style.css?version=1.001" type="text/css" />
+	<link rel="stylesheet" href="assets/css/style.css?version=1.001" type="text/css" />
 
 	
 	<script type="text/javavascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false"></script>
