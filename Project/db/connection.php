b5546b5b81991dc1b60465b3d66427cc7af247fe
Edwin Hattink <edwinhattink@me.com>
2014-02-18 10:36:03 +0100

    db naam naar develop

--- Project/db/connection.php
+++ Project/db/connection.php
@@ -4,7 +4,7 @@
 		$db_hostnaam = "212.83.232.237"; // De locatie waar de MySQL-service draait
 		$db_gebruiker = "confusingbucket"; // De gebruikersnaam waarmee naar de database kan worden verbonden
 		$db_wachtwoord = "bucket007"; // Het wachtwoord waarmee naar de database kan worden verbonden
-		$db_naam = "mydb"; // De naam van de database zelf, deze moet via de DBMS worden aangemaakt
+		$db_naam = "db_develop"; // De naam van de database zelf, deze moet via de DBMS worden aangemaakt
 		$mysqli = new mysqli($db_hostnaam, $db_gebruiker, $db_wachtwoord, $db_naam);
 		
 		if ($mysqli->connect_errno!= 0) //er gaat iets fout ...
