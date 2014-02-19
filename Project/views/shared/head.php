5fd324a161f56a22e628697b85d52e8f004843cf
Thomas Voogt <chirimorin@gmail.com>
2014-02-19 13:02:03 +0100

    Map separated

--- Project/views/shared/head.php
+++ Project/views/shared/head.php
@@ -8,26 +8,7 @@
 	<link rel="stylesheet" href="assets/css/style.css?version=1.001" type="text/css" />
 	<link href="assets/css/bootstrap.css" type="text/css" rel = "stylesheet" />
 
-	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false"></script>
-	
-	<script type="text/javascript">
-		var locations = new Array();
-		<?php
-			$locations = array();
-			
-			$locations[0] = (object) array('id' => '0', 'lat' => '51.688946', 'lng'=>'5.287256', 'title'=>'Avans hogeschool');
-			$locations[1] = (object) array('id' => '1', 'lat' => '51.67855', 'lng'=>'5.12173', 'title'=>'Robins huis');
-			
-			foreach ($locations as $location)
-			{
-				echo("locations.push( {  id:\"".$location->id."\", lat:".$location->lat.", lng:".$location->lng.", title:\"".$location->title."\" }); ");
-			}
-		?>
-	</script>
-	
 	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.js"></script>
-	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
-	<script type="text/javascript" src="assets/js/mapInit.js"></script>
 </head>
 	<body>
--- Project/views/shared/map.php
+++ Project/views/shared/map.php
@@ -1,1 +1,20 @@
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+
+<script type="text/javascript">
+	var locations = new Array();
+	<?php
+		$locations = array();
+		
+		$locations[0] = (object) array('id' => '0', 'lat' => '51.688946', 'lng'=>'5.287256', 'title'=>'Avans hogeschool');
+		$locations[1] = (object) array('id' => '1', 'lat' => '51.67855', 'lng'=>'5.12173', 'title'=>'Robins huis');
+		
+		foreach ($locations as $location)
+		{
+			echo("locations.push( {  id:\"".$location->id."\", lat:".$location->lat.", lng:".$location->lng.", title:\"".$location->title."\" }); ");
+		}
+	?>
+</script>
+
+<script type="text/javascript" src="assets/js/mapInit.js"></script>
+
 <div id="map-canvas"></div>
