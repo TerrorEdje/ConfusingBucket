8696c56e033921b88d51f5e6cba1989733995582
Jip <jip--pie@hotmail.com>
2014-02-19 20:05:38 +0100

    Klein beginnetje aan story detail pagina

--- Project/storylist_detail.php
+++ Project/storylist_detail.php
@@ -1,18 +1,33 @@
 <?php
 	include 'db/connection.php';
 	include 'repositories/storyRepository.php';
+	include 'repositories/typeRepository.php';
 	
 	$con = openDB();
 	
 	if($_GET['storyid'] != null)
 	{
 		$story = getStoryByID($_GET['storyid'], $con);
+		$type = getTypeByID($story -> _get("type_id"), $con);
 		
 		if($story != null)
 		{
-			echo "<h2>Stage gelopen van ".$story -> _get("startdate")." tot ".$story -> _get("enddate")."</h2>";
-			echo "<h3>".$story -> _get("description")."</h3>";
-			echo "<h3>Ik heb dit gedaan in leerjaar ".$story -> _get("schoolyear")."</h3>";
+			echo "Type: " .$type -> _get("name");
+			echo "Begindatum: " .$story -> _get("startdate");
+			echo "Einddatum: " .$story -> _get("startdate");
+			echo "Omschrijving: " .$story -> _get("description");
+			
+			echo "Student: ";
+			echo "Opleiding: ";
+			echo "Schooljaar: " .$story -> _get("schoolyear");
+			echo "School: ";
+			echo "Email: ";
+			
+			echo "Organisatie: ";
+			echo "Omschrijving organisatie: ";
+			echo "Locatie organisatie: ";
+			
+			echo "Verblijf: ";
 			echo "<h4><a href='".$story -> _get("link")."'>".$story -> _get("link")."</a></h4>";
 		}
 		else
