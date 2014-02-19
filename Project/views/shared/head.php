1bf4c7da757faa695d453e2fb2f0858d466b4fd0
RobinCollard <name24@hotmail.nl>
2014-02-19 13:11:44 +0100

    css version 2

--- Project/views/shared/head.php
+++ Project/views/shared/head.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
 	
-	<link rel="stylesheet" href="assets/css/style.css?version=1.001" type="text/css" />
+	<link rel="stylesheet" href="assets/css/style.css?version=1.002" type="text/css" />
 	<link href="assets/css/bootstrap.css" type="text/css" rel = "stylesheet" />
 
 	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
