8d1b163e4b64e497b002af60096d68b074f705c2
Thomas Voogt <chirimorin@gmail.com>
2014-02-22 15:02:06 +0100

    jQuery animate ipv handmatic

--- Project/assets/js/functions.js
+++ Project/assets/js/functions.js
@@ -13,55 +13,33 @@ function getHeight() {
   return myHeight;
 }
 
+function getMaxHeight()
+{
+    return getHeight() - 130;
+}
+
 hideshow = function()
 {	
-	var maxHeight = getHeight()-150;
-	var stepSize = maxHeight/30;
-	
 	if ($('#section').css('top') == "0px" || ($('#section').css('top') == ""))
 	{
-		var top = 0;
-		function frame2() 
-		{
-			top = top+stepSize;
-			if (top > maxHeight)
-			{
-				top = maxHeight;
-			}
-			$('#section').css('top',top + "px");
-			if (top >= maxHeight)
-			{
-				clearInterval(id)
-			}
-		}
-		var id = setInterval(frame2, 10) // draw every 10ms
-		
+        $('section').animate({
+            top: getMaxHeight() + "px"
+          }, 1500);
+        
 		$('#map_button_link').html("Click to hide map");
 	}
 	else
 	{
-		var top = maxHeight;;
-		function frame() 
-		{
-			top = top-stepSize;
-			if (top < 0)
-			{
-				top = 0;
-			}
-			$('#section').css('top',top + "px");
-			if (top <= 0)
-			{
-				clearInterval(id)
-			}
-		}
-		var id = setInterval(frame, 10) // draw every 10ms
-		
+        $('section').animate({
+            top: "0px"
+          }, 1500);
+        
 		$('#map_button_link').html("Click to show map");
 	}
 }
 
 $(window).on('resize', function(){
-	var maxHeight = getHeight()-150;
+	var maxHeight = getMaxHeight();
 	
 	if (!($('#section').css('top') == "0px" || ($('#section').css('top') == "")))
 	{
