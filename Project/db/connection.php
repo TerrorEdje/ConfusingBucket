19ebea60377710decec6b012d56a1bf6c0c0886a
Ruud <rdaelen@avans.nl>
2014-02-26 09:50:03 +0100

    connection.php fix

--- Project/db/connection.php
+++ Project/db/connection.php
@@ -1,7 +1,7 @@
 <?php	 
 	function openDB()
 	{
-		$db_hostname = "212.83.232.237"; // De locatie waar de MySQL-service draait
+		$db_hostname = "212.83.232.237:3306"; // De locatie waar de MySQL-service draait
 		$db_user = "confusingbucket"; // De usersname waarmee naar de database kan worden verbonden
 		$db_password = "bucket007"; // Het password waarmee naar de database kan worden verbonden
 		$db_name = "db_develop"; // De name van de database zelf, deze moet via de DBMS worden aangemaakt
