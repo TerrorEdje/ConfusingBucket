e354c76ee7fa508c7d8fd3dcf546410a01e94931
Ruud <rdaelen@avans.nl>
2014-02-26 13:23:23 +0100

    storylist begin

--- Project/repositories/storyRepository.php
+++ Project/repositories/storyRepository.php
@@ -12,9 +12,9 @@
 		while ($row =$result->fetch_assoc())
 		{
 			//alle waardes van tabel story zetten
-			$story[$i] = new Story();
+			$stories[$i] = new Story();
 			foreach ($row as $key => $value) {
-				$story[$i] -> _set($key, $value);
+				$stories[$i] -> _set($key, $value);
 			}
             
             $query2 = "SELECT * FROM study_has_student WHERE story_id='".$row["id"]."'";
@@ -28,8 +28,8 @@
                 $student_ids[$j] = $row["student_id"];
                 $j++;
             }
-            $story[$i] -> _set("study_id",$study_ids);
-            $story[$i] -> _set("student_ids",$student_ids);
+            $stories[$i] -> _set("study_id",$study_ids);
+            $stories[$i] -> _set("student_ids",$student_ids);
 			
 			$query3 = "SELECT * FROM organization_location WHERE story_id='".$row["id"]."'";
             $result3 = $connection->query($query3);
@@ -42,8 +42,8 @@
                 $location_ids[$k] = $row["location_id"];
                 $k++;
             }
-            $story[$i] -> _set("organization_ids",$organization_ids);
-            $story[$i] -> _set("location_ids",$location_ids);			
+            $stories[$i] -> _set("organization_ids",$organization_ids);
+            $stories[$i] -> _set("location_ids",$location_ids);			
 			$i++;
 		}
 		
