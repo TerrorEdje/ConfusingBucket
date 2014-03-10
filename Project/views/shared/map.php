2dafff4abd1fff3dfb3d7367320cb4693aca73a5
Thomas Voogt <chirimorin@gmail.com>
2014-03-10 10:35:46 +0100

    API key voor map

--- Project/views/shared/map.php
+++ Project/views/shared/map.php
@@ -3,7 +3,7 @@
 	include 'repositories/locationRepository.php';
 	$connection = openDB();
 ?>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&key=AIzaSyB4ofW1mgEVlMIoa48rMP0WkAksHAALU18"></script>
 
 <script type="text/javascript">
 	var locations = new Array();
