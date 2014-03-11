732dd78131ee983353ec372be2e14828a68d5607
Ruud <rdaelen@avans.nl>
2014-03-11 11:55:59 +0100

    in head een stylesheet toegevoegd. style.css aangepast: border van td weg en padding toegevoegd style en css van upload story gewijzigd

--- Project/upload_story.php
+++ Project/upload_story.php
@@ -25,34 +25,34 @@
 				echo '<option value ="' .$type->_get("id"). '">' .$type->_get("name"). '</option>';
 			}
 		echo '</select> *</td></tr>';
-		echo '<tr><td>Begindatum:</td> <td> <input type="text" name="startdate"> *</td></tr>'; # Moet datepicker worden
-		echo '<tr><td>Einddatum:</td> <td> <input type="text" name="enddate"> *</td></tr>'; # Moet datepicker worden
+		echo '<tr><td>Begindatum:</td> <td> <input type="text" name="startdate" class="customtextbox"> *</td></tr>'; # Moet datepicker worden
+		echo '<tr><td>Einddatum:</td> <td> <input type="text" name="enddate" class="customtextbox"> *</td></tr>'; # Moet datepicker worden
 		echo '<tr><td>&nbsp;</td></tr>';
 		echo '<tr><td>Gegevens over de organisatie:</td></tr>';
-		echo '<tr><td>Naam:</td> <td> <input type="text" name="name"> *</td></tr>';
-		echo '<tr><td>Omschrijving:</td> <td> <textarea name="description" ></textarea></td></tr>';
-		echo '<tr><td>Straat:</td> <td> <input type="text" name="streetname"></td></tr>';
-		echo '<tr><td>Huisnummer:</td> <td> <input type="text" name="number"></td></tr>';
-		echo '<tr><td>Postcode:</td> <td> <input type="text" name="zipcode"></td></tr>';
-		echo '<tr><td>Woonplaats:</td> <td> <input type="text" name="city"> *</td></tr>';
-		echo '<tr><td>Land:</td> <td> <input type="text" name="country"> *</td></tr>';
-		echo '<tr><td>Website:</td> <td> <input type="text" name="website"></td></tr>';
+		echo '<tr><td>Naam:</td> <td> <input type="text" name="name" class="customtextbox"> *</td></tr>';
+		echo '<tr><td>Omschrijving:</td> <td> <textarea name="description" class="customtextareadescription"></textarea></td></tr>';
+		echo '<tr><td>Straat:</td> <td> <input type="text" name="streetname" class="customtextbox"></td></tr>';
+		echo '<tr><td>Huisnummer:</td> <td> <input type="text" name="number" class="customtextbox"></td></tr>';
+		echo '<tr><td>Postcode:</td> <td> <input type="text" name="zipcode" class="customtextbox"></td></tr>';
+		echo '<tr><td>Woonplaats:</td> <td> <input type="text" name="city" class="customtextbox"> *</td></tr>';
+		echo '<tr><td>Land:</td> <td> <input type="text" name="country" class="customtextbox"> *</td></tr>';
+		echo '<tr><td>Website:</td> <td> <input type="text" name="website" class="customtextbox"></td></tr>';
 		echo '<tr><td>&nbsp;</td></tr>';
 		
 		echo '<tr><td>Schrijf hier over je ervaring:</td></tr>';
-		echo '<tr><td colspan="2"><textarea name="story" rows="10" cols="80"></textarea> *</td></tr>';
-		echo '<tr><td>Website(s): <input type="text" name="link"></td></tr>'; # Er moeten meerdere links ingevuld kunnen worden
+		echo '<tr><td colspan="2"><textarea name="story" class="customtextareastory"></textarea> *</td></tr>';
+		echo '<tr><td colspan="2">Website(s): <input type="text" name="link" class="customtextboxwebsite"></td></tr>'; # Er moeten meerdere links ingevuld kunnen worden
 		echo '<tr><td>&nbsp;</td></tr>';
 		
 		# Er hoeft geen verblijfplaast ingvuld te worden, maar als je dat wel doet, dan moeten woonplaats en land verplicht zijn
 		# Is het wel logisch om alleen een adres van de verblijfplaats in te vullen??
 	
 		echo '<tr><td>Gegevens over de verblijf locatie:</td></tr>';
-		echo '<tr><td>Straat:</td> <td> <input type="text" name="residence_streetname"></td></tr>';
-		echo '<tr><td>Huisnummer:</td> <td> <input type="text" name="residence_number"></td></tr>';
-		echo '<tr><td>Postcode:</td> <td> <input type="text" name="residence_zipcode"></td></tr>';
-		echo '<tr><td>Woonplaats:</td> <td> <input type="text" name="residence_city"></td></tr>';
-		echo '<tr><td>Land:</td> <td> <input type="text" name="residence_country"></td></tr>';
+		echo '<tr><td>Straat:</td> <td> <input type="text" name="residence_streetname" class="customtextbox"></td></tr>';
+		echo '<tr><td>Huisnummer:</td> <td> <input type="text" name="residence_number" class="customtextbox"></td></tr>';
+		echo '<tr><td>Postcode:</td> <td> <input type="text" name="residence_zipcode" class="customtextbox"></td></tr>';
+		echo '<tr><td>Woonplaats:</td> <td> <input type="text" name="residence_city" class="customtextbox"></td></tr>';
+		echo '<tr><td>Land:</td> <td> <input type="text" name="residence_country" class="customtextbox"></td></tr>';
 
 		echo '<tr><td><input type="submit" name="upload" value="Upload story"></td></tr>';
 		
--- Project/views/shared/head.php
+++ Project/views/shared/head.php
@@ -9,6 +9,7 @@
 	
 	<link rel="stylesheet" href="assets/css/style.css?version=1.002" type="text/css" />
 	<link rel="stylesheet" href="assets/css/StoryListStyle.css?version=1.000" type="text/css" />
+	<link rel="stylesheet" href="assets/css/inputStyles.css?version=1.000" type="text/css" />
 	<link href="assets/css/bootstrap.css" type="text/css" rel = "stylesheet" />
 
 	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
