fdb48a0c2be919988d8e1bea0809778871bb1b4c
Edwin Hattink <edwinhattink@me.com>
2014-03-20 10:53:01 +0100

    story

--- Larakaart/app/models/Story.php
+++ Larakaart/app/models/Story.php
@@ -11,44 +11,5 @@ class Story extends Eloquent implements StoryInterface, RemindableInterface {
 	 * @var string
 	 */
 	protected $table = 'story';
-
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password');
-
-	/**
-	 * Get the unique identifier for the Story.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
-
-	/**
-	 * Get the password for the Story.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
-
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
-
 }
-?>
 ?>
