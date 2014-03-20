21a50c9ab6dcbd1e4824b9b54947de69487cf792
RobinCollard <name24@hotmail.nl>
2014-03-20 10:13:59 +0100

    Push storylist begin

---
+++ Larakaart/app/controllers/StorylistController.php
@@ -0,0 +1,15 @@
+<?php
+
+class IndexController extends BaseController {
+
+	protected $layout = 'layout.main';
+
+	public function index()
+	{
+		//$this->layout->content = View::make('home');
+		return View::make('storylist');
+	}
+
+}
+
+?>
