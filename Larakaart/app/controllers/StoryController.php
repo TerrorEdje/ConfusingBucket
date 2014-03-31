7e9404a48bd562df79e88abe830d6a6843c453e1
Ruud <rdaelen@avans.nl>
2014-03-31 10:20:37 +0200

    Swegg ik weet niet wat er is aangepast :)

--- Larakaart/app/controllers/StoryController.php
+++ Larakaart/app/controllers/StoryController.php
@@ -12,8 +12,22 @@ class StoryController extends BaseController {
 	public function storydetail($id)
 	{
 		$story = Story::find($id);
+		$student = Student::find($story->student_id);
+		$study = Study::find($story->study_id);
+		$organization = Organization::find($story->organization_id);
+		$school = School::find($study->school_id);
+		$storylocations = Storylocation::where('story_id', $id);
+		$locations = array();
+		echo "$storylocations";
+		foreach ($storylocations as $storylocation)
+		{
+		
+			$location = Location::find($storylocation->location_id);
+			array_push($locations,$location);
+		}
+		
 	
-		return View::make('storydetail')->with('story', $story);
+		return View::make('storydetail')->with('story', $story)->with('student', $student)->with('study', $study)->with('organization', $organization)->with('school', $school)->with('locations', $locations);
 	}
 
 }
--- Larakaart/app/views/storydetail.blade.php
+++ Larakaart/app/views/storydetail.blade.php
@@ -1,7 +1,31 @@
 @extends('layout.main')
 
-@section('content')
-	<p><h3>Storylistdetail</h3></p>
-	<p>Mijn ID = {{ $story['id'] }}</p>
+@section('content')	
+	<table>
+		<tr><td>Type: </td><td>{{ $story['type'] }}</td></tr>
+		
+		<tr><td style="width: 160pxpx;">Startdate: </td><td>{{ $story['startdate'] }}</td></tr>
+		<tr><td style="width: 160px;">Enddate: </td><td>{{ $story['enddate'] }}</td></tr>
+		<tr><td colspan="2">&nbsp;</td></tr>
+		<tr><td style="width: 160px;">Description: </td><td>{{ $story['description'] }}</td></tr>
+		<tr><td colspan="2">&nbsp;</td></tr>
+		<tr><td style="width: 160px;">Student: </td><td>{{$student['firstname']}} {{$student['insertion']}} {{$student['surname']}}</td></tr>
+		<tr><td style="width: 160px;">Study: </td><td>{{$study['name']}}</td></tr>
+		<tr><td style="width: 160px;">Schoolyear: </td><td>{{ $story['schoolyear'] }}</td></tr>
+		<tr><td style="width: 160px;">School: </td><td>{{$school['name']}}</td></tr>
+		<tr><td style="width: 160px;">Email: </td><td>{{$student['email']}}</td></tr>
+		<tr><td colspan="2">&nbsp;</td></tr>
+		<tr><td style="width: 160px;">Organization: </td><td>{{$organization['name']}}</td></tr>
+		<tr><td style="width: 160px;">Organization description: </td><td>{{$organization['description']}}</td></tr>
+		<tr><td style="width: 160px;">Website: </td><td><a href="http://{{$organization['website']}}" target=\"_blank\">{{$organization['website']}}</a></td></tr>
+		<tr><td colspan="2">&nbsp;</td></tr>
+		@foreach($locations as $location)
+			<tr><td class="tdTop">Locatie(s) story: </td><td><ul>
+			<li>TYPE<br/>{{$location['streetname']}} {{$location['number']}}<br/>{{$location['zipcode']}} {{$location['city']}}<br/>{{$location['country']}}</li>
+			</ul></td></tr>
+		@endforeach
+		
+	</table>
+
 	
 @stop
