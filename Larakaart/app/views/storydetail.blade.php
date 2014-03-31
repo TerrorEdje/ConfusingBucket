cc067ee60f1154d5bd8fdd60de427951038b7e21
Edwin Hattink <edwinhattink@me.com>
2014-03-31 12:00:42 +0200

    layout uit storydetail gehaald (width dingetjes)

--- Larakaart/app/views/storydetail.blade.php
+++ Larakaart/app/views/storydetail.blade.php
@@ -4,21 +4,21 @@
 	<table>
 		<tr><td>Type: </td><td>{{ $story['type'] }}</td></tr>
 		
-		<tr><td style="width: 160pxpx;">Startdate: </td><td>{{ $story['startdate'] }}</td></tr>
-		<tr><td style="width: 160px;">Enddate: </td><td>{{ $story['enddate'] }}</td></tr>
-		<tr><td colspan="2">&nbsp;</td></tr>
-		<tr><td style="width: 160px;">Description: </td><td>{{ $story['description'] }}</td></tr>
-		<tr><td colspan="2">&nbsp;</td></tr>
-		<tr><td style="width: 160px;">Student: </td><td>{{$student['firstname']}} {{$student['insertion']}} {{$student['surname']}}</td></tr>
-		<tr><td style="width: 160px;">Study: </td><td>{{$study['name']}}</td></tr>
-		<tr><td style="width: 160px;">Schoolyear: </td><td>{{ $story['schoolyear'] }}</td></tr>
-		<tr><td style="width: 160px;">School: </td><td>{{$school['name']}}</td></tr>
-		<tr><td style="width: 160px;">Email: </td><td>{{$student['email']}}</td></tr>
-		<tr><td colspan="2">&nbsp;</td></tr>
-		<tr><td style="width: 160px;">Organization: </td><td>{{$organization['name']}}</td></tr>
-		<tr><td style="width: 160px;">Organization description: </td><td>{{$organization['description']}}</td></tr>
-		<tr><td style="width: 160px;">Website: </td><td><a href="http://{{$organization['website']}}" target=\"_blank\">{{$organization['website']}}</a></td></tr>
-		<tr><td colspan="2">&nbsp;</td></tr>
+		<tr><td>Startdate: </td><td>{{ $story['startdate'] }}</td></tr>
+		<tr><td>Enddate: </td><td>{{ $story['enddate'] }}</td></tr>
+		<tr><td>&nbsp;</td></tr>
+		<tr><td>Description: </td><td>{{ $story['description'] }}</td></tr>
+		<tr><td>&nbsp;</td></tr>
+		<tr><td>Student: </td><td>{{$student['firstname']}} {{$student['insertion']}} {{$student['surname']}}</td></tr>
+		<tr><td>Study: </td><td>{{$study['name']}}</td></tr>
+		<tr><td>Schoolyear: </td><td>{{ $story['schoolyear'] }}</td></tr>
+		<tr><td>School: </td><td>{{$school['name']}}</td></tr>
+		<tr><td>Email: </td><td>{{$student['email']}}</td></tr>
+		<tr><td>&nbsp;</td></tr>
+		<tr><td>Organization: </td><td>{{$organization['name']}}</td></tr>
+		<tr><td>Organization description: </td><td>{{$organization['description']}}</td></tr>
+		<tr><td>Website: </td><td><a href="http://{{$organization['website']}}" target=\"_blank\">{{$organization['website']}}</a></td></tr>
+		<tr><td>&nbsp;</td></tr>
 		@for ($i = 0; $i < count($locations); $i ++)
 			<tr><td class="tdTop">Locatie(s) story: </td><td><ul><li>
 			{{$storylocations[$i]['location_type']}}<br/>
