07b8b56a4ea1d2bf562007e5fce720e0035d38aa
Edwin Hattink <edwinhattink@me.com>
2014-04-08 12:21:07 +0200

    home tekst veranderd.

--- Larakaart/app/views/home.blade.php
+++ Larakaart/app/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.main')
 
 @section('content')
-	<p>hee hallow</p>
+	<h1>Welcome to <span class="text-primary">the Worldmap</span> of Confusing Bucket!</h1>
+	<br />
+	<h3>On this website you can read stories by students who went to do a minor, internship or graduation in foreign countries.
+	You will find information about residence locations, classes and way more. 
+	By using the map on the background you can find the stories of students.</h3>
 @stop
