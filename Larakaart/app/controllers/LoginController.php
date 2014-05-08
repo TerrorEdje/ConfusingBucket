f916ab2c3ec63a0ac93661d4dde69046c9a6b829
RobinCollard <name24@hotmail.nl>
2014-05-08 11:54:29 +0200

    function voor redirect van Google.

--- Larakaart/app/controllers/LoginController.php
+++ Larakaart/app/controllers/LoginController.php
@@ -14,39 +14,44 @@ class LoginController extends BaseController {
 
 	public function loginWithGoogle() {
 
-    // get data from input
-    $code = Input::get( 'code' );
+	    // get data from input
+	    $code = Input::get( 'code' );
 
-    // get google service
-    $googleService = OAuth::consumer( 'Google' );
+	    // get google service
+	    $googleService = OAuth::consumer( 'Google' );
 
-    // check if code is valid
+	    // check if code is valid
 
-    // if code is provided get user data and sign in
-    if ( !empty( $code ) ) {
+	    // if code is provided get user data and sign in
+	    if ( !empty( $code ) ) {
 
-        // This was a callback request from google, get the token
-        $token = $googleService->requestAccessToken( $code );
+	        // This was a callback request from google, get the token
+	        $token = $googleService->requestAccessToken( $code );
 
-        // Send a request with it
-        $result = json_decode( $googleService->request( 'https://www.googleapis.com/oauth2/v1/userinfo' ), true );
+	        // Send a request with it
+	        $result = json_decode( $googleService->request( 'https://www.googleapis.com/oauth2/v1/userinfo' ), true );
 
-        $message = 'Your unique Google user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
-        echo $message. "<br/>";
+	        $message = 'Your unique Google user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
+	        echo $message. "<br/>";
 
-        //Var_dump
-        //display whole array().
-        dd($result);
+	        //Var_dump
+	        //display whole array().
+	        dd($result);
 
-    }
-    // if not ask for permission first
-    else {
-        // get googleService authorization
-        $url = $googleService->getAuthorizationUri();
+	    }
+	    // if not ask for permission first
+	    else {
+	        // get googleService authorization
+	        $url = $googleService->getAuthorizationUri();
 
-        // return to facebook login url
-        return Redirect::to( (string)$url );
-    }
-}
+	        // return to facebook login url
+	        return Redirect::to( (string)$url );
+    	}
+	}
+
+	public function loggedInWithGoogle() {
+
+		return '<pre>print_r($1)</pre>';
+	}
 
 }
