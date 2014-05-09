a649368dd1a6e4ce7196cb1719085620133f52a9
thimh <thim.heider@gmail.com>
2014-05-09 15:29:35 +0200

    Tests (aangepast/gemaakt)

--- Larakaart/app/views/organizationlist.blade.php
+++ Larakaart/app/views/organizationlist.blade.php
@@ -5,7 +5,7 @@
 				<div class="panel-heading">
 					<h1 class="panel-title"> 
 						<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#{{ $organization['id']}}">
-							<i class="indicator glyphicon glyphicon-chevron-down blue"></i>
+							<i class="indicator glyphicon glyphicon-chevron-down blue"></i>&nbsp;&nbsp;&nbsp;
 							{{ $organization['name'] }} ({{ $organization->getLocation()->city }}, {{ $organization->getLocation()->country }})		
 						</a>
 					</h1>
@@ -28,17 +28,3 @@
         $('#accordion').accordionChevrons();
     });
 </script>
-
-
-
-	<!-- <table class="tblUserStory" rules='cols'>
-	<tr ><th>Name</th><th class="otherTDTH">Type</th><th class="otherTDTH">Website</th><th class="otherTDTH">Link</th></tr>
-	@foreach ($organizations as $organization)
-		<tr>
-			<td class="otherTDTH">{{ $organization['name'] }}</td>
-			<td class="otherTDTH">{{ $organization['type'] }}</td>
-			<td class="otherTDTH">{{ $organization['website'] }}</td>
-			<td class="otherTDTH"><a href="#" onclick="load('{{ URL::route('organizationdetail', array($organization['id'])) }}','organizationdetailmenu'); return false;">Detail</a></td>
-		</tr>
-	@endforeach
-	</table> -->
