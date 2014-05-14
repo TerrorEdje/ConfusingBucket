a4ef77bf640b0fff2add49049685e0db2735d418
Ruud <rdaelen@avans.nl>
2014-05-14 15:05:44 +0200

    Layout activity uploaden

--- Larakaart/app/views/activity/upload.blade.php
+++ Larakaart/app/views/activity/upload.blade.php
@@ -1,5 +1,12 @@
+<script>   
+	$(function() {
+		$( "#calendar" ).datepicker({ dateFormat: 'dd-mm-yy' });
+		$( "#calendar2" ).datepicker({ dateFormat: 'dd-mm-yy' });   
+	}); 
+</script>
 
-	{{ Form::open(array('url' => 'activity/add'), 'post') }}
+
+<!--	{{ Form::open(array('url' => 'activity/add'), 'post') }}
 	
 		Information about the activity:</br>
 		
@@ -32,4 +39,44 @@
 		
 		{{Form::submit('Upload Activity')}}
 		{{Form::token()}}
-	{{ Form::close() }}
+	{{ Form::close() }}-->
+	
+{{ Form::open(array('url' => 'activity/add'), 'post') }}
+	<fieldset class="the-fieldset form-margin">
+		<legend class="the-legend text-primary">Information about the activity:</legend>
+		<table>
+			<tr>
+				<td style="width: 150px"><label class="text-primary">Organization: </label></td><td>{{Form::select('organization',$organizations, Input::old('name'), array('class'=>'form-control')) }}</td>
+				<td style="width: 150px">&nbsp;</td>
+				<td style="width: 150px"><label class="text-primary">Startdate: </label></td><td>{{ Form::text('startdate', null, array('class' => 'form-control','placeholder' => 'Startdate', 'id' => 'calendar')) }}</td>
+			</tr>
+			<tr><td colspan="5">&nbsp;</td></tr>
+			<tr>
+				<td style="width: 150px"><label class="text-primary">Type: </label></td><td>{{Form::select('type', $types, Input::old('name'), array('class'=>'form-control')) }}</td>
+				<td style="width: 50px">&nbsp;</td>
+				<td style="width: 150px"><label class="text-primary">Enddate: </label></td><td>{{ Form::text('enddate', null, array('class' => 'form-control','placeholder' => 'Enddate', 'id' => 'calendar2')) }}</td>
+			</tr>
+			<tr><td colspan="5">&nbsp;</td></tr>
+			<tr>
+				<td style="width: 150px"><label class="text-primary">Name: </label></td><td>{{ Form::text('name', null, array('class' => 'form-control','placeholder' => 'Name')) }}</td>
+			</tr>
+			<tr><td colspan="5">&nbsp;</td></tr>
+			<tr>
+				<td style="width: 150px"><label class="text-primary">Study: </label></td><td>{{Form::select('study', $studies, Input::old('name'), array('class'=>'form-control')) }}</td>
+				<td style="width: 50px">&nbsp;</td>
+				<td style="width: 150px"><label class="text-primary">Status: </label></td><td>{{Form::select('status', $statuses, Input::old('name'), array('class'=>'form-control')) }}</td>
+			</tr>
+			<tr><td colspan="5">&nbsp;</td></tr>
+			<tr>
+				<td style="width: 150px"><label class="text-primary">Description: </label></td>
+			</tr>
+			<tr>
+				<td colspan="2">{{ Form::textarea('description',null, array('class' => 'form-control uploadActivityTextarea', 'rows' => '10')) }}</td>
+			</tr>
+			<tr><td colspan="5">&nbsp;</td></tr>
+		</table>
+	{{Form::submit('Upload Activity', array('class' => 'btn btn-success'))}}
+	</fieldset>
+	{{Form::token()}}
+{{ Form::close() }}	
+	
