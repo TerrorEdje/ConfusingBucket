b73902b2798079528bd96e103b20da8e647a7217
michael van de Ven <michael.vd.ven@hotmail.com>
2014-05-15 13:13:20 +0200

    test

--- Larakaart/app/views/layout/shared/footer.blade.php
+++ Larakaart/app/views/layout/shared/footer.blade.php
@@ -1,3 +1,10 @@
 	<footer><p>&copy; - Confusing Bucket</p></footer>
+
+	<div class="fullpage">
+		<div class="load">
+			<h1 class="primary">Gegevens worden geverifieerd bij Google.</h1>
+			
+		</div>
+	</div>
 </body>
 </html>
