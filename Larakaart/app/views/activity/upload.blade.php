86a7648e4454a1132bf767c6f16067551056783e
Thomas Voogt <chirimorin@gmail.com>
2014-05-15 15:47:04 +0200

    Gele update knoppen

--- Larakaart/app/views/activity/upload.blade.php
+++ Larakaart/app/views/activity/upload.blade.php
@@ -5,42 +5,6 @@
 	}); 
 </script>
 
-
-<!--	{{ Form::open(array('url' => 'activity/add'), 'post') }}
-	
-		Information about the activity:</br>
-		
-		<p>
-			Organization: 	{{ Form::select('organization', $organizations,Input::old('name')) }} 
-							{{ $errors->first('organization', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}</br>
-							
-			Type: 			{{ Form::select('type', $types, Input::old('name')) }} 
-							{{ $errors->first('type', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}</br>
-				  
-			Name: 			{{ Form::text('name') }} 
-							{{ $errors->first('name', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}</br>
-				  
-			Description: </br>
-							{{ Form::textarea('description') }} 
-							{{ $errors->first('description', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}</br>
-			
-			Startdate: 		{{ Form::text('startdate') }} 
-							{{ $errors->first('startdate', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}</br>
-					   
-			Enddate: 		{{ Form::text('enddate') }} 
-							{{ $errors->first('enddate', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}</br>
-					 
-			Study: 			{{ Form::select('study', $studies,Input::old('name')) }} 
-							{{ $errors->first('study', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}</br>
-				   
-			Status: 		{{ Form::select('status', $statuses,Input::old('name')) }} 
-							{{ $errors->first('status', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}</br>
-		</p>
-		
-		{{Form::submit('Upload Activity')}}
-		{{Form::token()}}
-	{{ Form::close() }}-->
-	
 {{ Form::open(array('url' => 'activity/add', 'post', 'class'=>'form-horizontal')) }}
 	<fieldset class="the-fieldset form-margin">
 		<legend class="the-legend text-primary">Information about the activity:</legend>
@@ -78,7 +42,7 @@
             <div class="form-group">
                 <label for="name" class="col-sm-3 control-label text-primary">Study: </label>
                 <div class="col-sm-9">
-                    {{Form::select('study', $studies, Input::old('name'), array('class'=>'form-control')) }}
+                    {{Form::select('study', $studies, Input::old('study'), array('class'=>'form-control')) }}
                 </div>
                 <div class="col-sm-offset-3 col-sm-9 has-error">
                     {{ $errors->first('study', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}
