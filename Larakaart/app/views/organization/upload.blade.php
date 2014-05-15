360eb1aba73648fa1ac8eb02cd6033a2ee5d6ae0
Edwin Hattink <edwinhattink@me.com>
2014-05-15 13:30:21 +0200

    upload doet het

--- Larakaart/app/views/organization/upload.blade.php
+++ Larakaart/app/views/organization/upload.blade.php
@@ -3,62 +3,46 @@
 
 <form action="{{ URL::route('Organization-upload-add') }}" method="post" class="form-horizontal">
 	<fieldset class="the-fieldset form-margin">
-   		<legend class="the-legend text-primary">Organization</legend>
-	
+   		<legend class="the-legend text-primary">Organization</legend>		
+		
 		<div class="form-group">
 	    	<label for="name" class="col-sm-2 control-label">Name: </label>
 			<div class="col-sm-8">
-			   	<input type="text" class="form-control" id="name" name="name" {{ (Input::old('name')) ? 'value="'.e(Input::old('name')).'"': '' }}>
+			   	{{Form::text('name', null, array('class' => 'form-control','placeholder' => 'Name'))}}
 			</div>
-			@if($errors->has('name'))
-				<div class="col-sm-offset-2 col-sm-8 has-error">
-					<h5 class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> {{ $errors->first('name')}}</h5>
-				</div>
-			@endif
+			<div class="col-sm-offset-2 col-sm-8 has-error">
+				{{ $errors->first('name', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}
+			</div>
 	  	</div>
         
         <div class="form-group">
 	    	<label for="type" class="col-sm-2 control-label">Type: </label>
 			<div class="col-sm-8">
-			   	<select class="form-control" id="type" name="type">
-					@foreach($types as $type)
-						@if($type == 'Select...')
-							<option>{{$type}}</option>
-						@else
-							<option value="{{$type}}">{{$type}}</option>
-						@endif
-					@endforeach
-				</select>
+			   	{{Form::select('type',$types, Input::old('type'), array('class'=>'form-control'))}}
 			</div>
-			@if($errors->has('type'))
-				<div class="col-sm-offset-2 col-sm-8 has-error">
-					<h5 class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> {{ $errors->first('type')}}</h5>
-				</div>
-			@endif
+			<div class="col-sm-offset-2 col-sm-8 has-error">
+				{{ $errors->first('type', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}
+			</div>
 	  	</div>
 
 	  	<div class="form-group">
 	    	<label for="website" class="col-sm-2 control-label">Website: </label>
 	   		<div class="col-sm-8">
-	      		<input type="text" class="form-control" id="website" name="website" {{ (Input::old('website')) ? 'value="'.e(Input::old('website')).'"': '' }}>
+	      		{{Form::text('website', null, array('class' => 'form-control','placeholder' => 'Website'))}}
 	    	</div>
-	    	@if($errors->has('website'))
-				<div class="col-sm-offset-2 col-sm-8 has-error">
-					<h5 class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> {{ $errors->first('website')}}</h5>
-				</div>
-			@endif
+			<div class="col-sm-offset-2 col-sm-8 has-error">
+				{{ $errors->first('website', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}
+			</div>
 	  	</div>
         
         <div class="form-group">
 	    	<label for="description" class="col-sm-2 control-label">Description: </label>
 	   		<div class="col-sm-8">
-                <textarea name="description" class="form-control" id="description" cols="50" rows="10">{{ (Input::old('description')) ? 'value="'.e(Input::old('description')).'"': '' }}</textarea>
+                {{Form::textarea('description', null, array('class' => 'form-control','placeholder' => 'Description'))}}
 	    	</div>
-	    	@if($errors->has('description'))
-				<div class="col-sm-offset-2 col-sm-8 has-error">
-					<h5 class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> {{ $errors->first('description')}}</h5>
-				</div>
-			@endif
+			<div class="col-sm-offset-2 col-sm-8 has-error">
+				{{ $errors->first('description', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}
+			</div>
 	  	</div>
         
         <legend class="the-legend text-primary">Address</legend>
@@ -66,62 +50,53 @@
         <div class="form-group">
 	    	<label for="streetname" class="col-sm-2 control-label">Street: </label>
 			<div class="col-sm-6">
-			   	<input type="text" class="form-control address" id="streetname" name="streetname" {{ (Input::old('streetname')) ? 'value="'.e(Input::old('streetname')).'"': '' }}>
+			   	{{Form::text('streetname', null, array('class' => 'form-control','placeholder' => 'Street'))}}
 			</div>
-			@if($errors->has('streetname'))
-				<div class="col-sm-offset-2 col-sm-6 has-error">
-					<h5 class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> {{ $errors->first('streetname')}}</h5>
-				</div>
-			@endif
+			<div class="col-sm-offset-2 col-sm-6 has-error">
+				{{ $errors->first('streetname', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}
+			</div>
+			
             <label for="number" class="col-sm-1 control-label">No: </label>
 			<div class="col-sm-1">
-			   	<input type="text" class="form-control address" id="number" name="number" {{ (Input::old('number')) ? 'value="'.e(Input::old('number')).'"': '' }}>
+			   	{{Form::text('number', null, array('class' => 'form-control','placeholder' => 'Number'))}}
 			</div>
-			@if($errors->has('number'))
-				<div class="col-sm-offset-8 col-sm-1 has-error">
-					<h5 class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> {{ $errors->first('number')}}</h5>
-				</div>
-			@endif
+			<div class="col-sm-offset-8 col-sm-1 has-error">
+				{{ $errors->first('number', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}
+			</div>
 	  	</div>
         
         <div class="form-group">
 	    	<label for="city" class="col-sm-2 control-label">City: </label>
 			<div class="col-sm-8">
-			   	<input type="text" class="form-control address" id="city" name="city" {{ (Input::old('city')) ? 'value="'.e(Input::old('city')).'"': '' }}>
+			   	{{Form::text('city', null, array('class' => 'form-control','placeholder' => 'City'))}}
 			</div>
-			@if($errors->has('city'))
-				<div class="col-sm-offset-2 col-sm-8 has-error">
-					<h5 class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> {{ $errors->first('city')}}</h5>
-				</div>
-			@endif
+			<div class="col-sm-offset-2 col-sm-8 has-error">
+				{{ $errors->first('city', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}
+			</div>
 	  	</div>
         
         <div class="form-group">
 	    	<label for="zipcode" class="col-sm-2 control-label">Zipcode: </label>
 			<div class="col-sm-8">
-			   	<input type="text" class="form-control" id="zipcode" name="zipcode" {{ (Input::old('zipcode')) ? 'value="'.e(Input::old('zipcode')).'"': '' }}>
+			   	{{Form::text('zipcode', null, array('class' => 'form-control','placeholder' => 'Zipcode'))}}
 			</div>
-			@if($errors->has('zipcode'))
-				<div class="col-sm-offset-2 col-sm-8 has-error">
-					<h5 class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> {{ $errors->first('zipcode')}}</h5>
-				</div>
-			@endif
+			<div class="col-sm-offset-2 col-sm-8 has-error">
+				{{ $errors->first('zipcode', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}
+			</div>
 	  	</div>
         
         <div class="form-group">
 	    	<label for="country" class="col-sm-2 control-label">Country: </label>
 			<div class="col-sm-8">
-			   	<input type="text" class="form-control address" id="country" name="country" {{ (Input::old('country')) ? 'value="'.e(Input::old('country')).'"': '' }}>
+			   	{{Form::text('country', null, array('class' => 'form-control','placeholder' => 'Country'))}}
 			</div>
-			@if($errors->has('country'))
-				<div class="col-sm-offset-2 col-sm-8 has-error">
-					<h5 class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> {{ $errors->first('country')}}</h5>
-				</div>
-			@endif
+			<div class="col-sm-offset-2 col-sm-8 has-error">
+				{{ $errors->first('country', '<span class="text-danger"><span class="glyphicon glyphicon-remove form-control-feedback"></span> :message</span>') }}
+			</div>
 	  	</div>
         
         <div class="form-group">
-	    	<label for="country" class="col-sm-2 control-label">Location: </label>
+	    	<label for="location" class="col-sm-2 control-label">Location: </label>
 			<div class="col-sm-8">
 			   	<div id="location-canvas" style="width: 100%; height: 300px;"></div>
             </div>
