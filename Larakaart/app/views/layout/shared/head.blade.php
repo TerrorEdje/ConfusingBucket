fda5e24b0f5040ae6c8058faec5fff699c2cd6f6
Thomas Voogt <chirimorin@gmail.com>
2014-06-17 11:56:30 +0200

    favicons

--- Larakaart/app/views/layout/shared/head.blade.php
+++ Larakaart/app/views/layout/shared/head.blade.php
@@ -4,6 +4,27 @@
 	<title>Wereldkaart</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    
+    <!-- ****** favicons ****** -->
+	<link rel="shortcut icon" href="assets/favicons/favicon.ico">
+	<link rel="icon" sizes="16x16 32x32 64x64" href="assets/favicons/favicon.ico">
+	<link rel="icon" type="image/png" sizes="196x196" href="assets/favicons/favicon-196.png">
+	<link rel="icon" type="image/png" sizes="160x160" href="assets/favicons/favicon-160.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/favicons/favicon-96.png">
+	<link rel="icon" type="image/png" sizes="64x64" href="assets/favicons/favicon-64.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="assets/favicons/favicon-32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="assets/favicons/favicon-16.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="assets/favicons/favicon-152.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="assets/favicons/favicon-144.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="assets/favicons/favicon-120.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/favicons/favicon-114.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/favicons/favicon-76.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/favicons/favicon-72.png">
+	<link rel="apple-touch-icon" href="assets/favicons/favicon-57.png">
+	<meta name="msapplication-TileColor" content="#FFFFFF">
+	<meta name="msapplication-TileImage" content="assets/favicons/favicon-144.png">
+	<meta name="msapplication-config" content="assets/favicons/browserconfig.xml">
+	<!-- ****** favicons ****** -->
 	
 	{{HTML::style('assets/css/style.css?version=1.002')}}
 	{{HTML::style('assets/css/StoryListStyle.css?version=1.000')}}
