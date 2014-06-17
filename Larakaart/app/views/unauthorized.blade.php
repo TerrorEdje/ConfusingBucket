4f51b85adc11a11bfcefaafebffb1dc47a629961
thimh <thim.heider@gmail.com>
2014-06-17 14:26:00 +0200

    unauthorized.blade.php MOET NOG NAAR GELINKT WORDEN!!!!!!

---
+++ Larakaart/app/views/unauthorized.blade.php
@@ -0,0 +1,5 @@
+<h3><span class='text-danger'>You are not authorized to access this page.</span></h3>
+
+<script type="text/javascript"> 
+	$('#breadcrumb').html('<a href="#" onclick="load(\'./?nolayout\', \'homemenu\'); return false;">Home</a> &raquo; CMS');
+</script>
